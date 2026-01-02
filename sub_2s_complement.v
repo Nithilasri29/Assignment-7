e365d45077cb9739ba4b2393c0bf6b28e0f4f3e3
Nithilasri29 <nithilasri29@gmail.com>
2026-01-02 20:42:28 +0530

    Update sub_2s_complement.v

--- sub_2s_complement.v
+++ sub_2s_complement.v
@@ -8,14 +8,11 @@ module subtract_2s_complement (
     wire [3:0] B_comp;
     wire [4:0] Sum;
 
-    // Step 1 & 2: 2's complement of B
+   
     assign B_comp = ~B + 1'b1;
-
-    // Step 3: Add A and 2's complement of B
     assign Sum = A + B_comp;
-
-    // Step 4: Assign result
     assign Result = Sum[3:0];
     assign Carry  = Sum[4];   // Carry = 1 ? A ? B
 
 endmodule
+
